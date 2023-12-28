8c22f4073eb69614455a8605072ca9b47f20749e
sofianzlitni <sofienzlitni@gmail.com>
2023-12-28 02:00:35 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(express.static("public"));
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors({ origin: true, credentials: true }));
+
+const stripe = require("stripe")(
+  "sk_test_51NnftvCMYyQVBx6m6ZXRaezrZ0f8DCe6tWKqFBBDImRqUFiqWM7MYkxxgf2FjdY1H343DmQlz5QLEYC3EbL3G4VP00qsrVmV1I"
+);
+
+app.post("/checkout", async (req, res, next) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: req.body.items.map((item) => ({
+        price_data: {
+            currency: "usd",
+            product_data: {
+                name: item.name,
+                images: [item.product],
+            },
+            unit_amount: item.price *  100
+        },
+        quantity: item.quantity
+      })),
+      mode: "payment",
+      success_url: "http://localhost4242/success.html",
+      cancel_url: "http://localhost4242/cancel.html",
+    });
+
+    res.status(200).json(session);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(4242, () => console.log("app is running on 4242"));
